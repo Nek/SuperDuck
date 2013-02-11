c63a13ce0ad42e38994e490a9a77806ee2577f0f
Nikita Dudnik aka Nek <nikdudnik@gmail.com>
2013-02-12 04:41:54 +0700

    Add user classes check. Add Boolean check.

--- test/superduck_test.js
+++ test/superduck_test.js
@@ -64,12 +64,12 @@ this.superduck_test = {
         test.done();
     },
     'match' : function(test) {
-        test.expect(17);
+        test.expect(23);
 
         var sd = superduck();
         var $ = sd.$;
         var is = sd.is;
-        
+
         sd.matcher({a:1})({a:1},function(r){
             test.ok(r, "should match");
         });
@@ -138,10 +138,41 @@ this.superduck_test = {
             test.ok(r === true, "should match");
         });
 
+        sd.matcher({b:Number})({b:1},function(r){
+            test.ok(r === true, "should match");
+        });
+
+        sd.matcher({b:String})({b:1},function(r){
+            test.ok(r === false, "shouldn't match");
+        });
+
+        sd.matcher({b:String})({b:"!!!"},function(r){
+            test.ok(r === true, "should match");
+        });
+
+        function Apple() {
+
+        }
+
+        is.register(Apple);
+
+
+        sd.matcher({b:Apple})({b:Apple},function(r){
+            test.ok(r === false, "shouldn't match");
+        });
+
+        sd.matcher({b:Apple})({b:new Apple()},function(r){
+            test.ok(r === true, "should match");
+        });
+
+        sd.matcher({b:Boolean})({b:true},function(r){
+            test.ok(r === true, "should match");
+        });
+
         test.done();
     },
     'is' : function(test) {
-        test.expect(8);
+        test.expect(11);
         var sd = superduck();
         var is = sd.is;
         test.ok(is.Array([]), "It's an Array");
@@ -157,6 +188,9 @@ this.superduck_test = {
         test.ok(is.Function(a), "It's a function");
         test.ok(is.Function(1) === false, "It's NOT a function");
         test.ok(is.Function(new Function("return null")), "It's a function");
+        test.ok(is.Boolean(true) === true, "true is Boolean");
+        test.ok(is.Boolean(false) === true, "false is Boolean");
+        test.ok(is.Boolean("true") !== true, "String is not Boolean");
         test.done();
 
     }
